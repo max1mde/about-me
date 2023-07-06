741e40222c855c61bc6473fddb3d54eed82eebac
Maxim <maximdeclient@gmail.com>
2023-07-06 20:23:37 +0200

    Fixed routing bug for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import HomePage from './pages/HomePage.tsx';
 import ContactPage from './pages/ContactPage.tsx';
 import ProjectsPage from './pages/ProjectsPage.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
-import { Route, Routes } from "react-router";
-import {useLocation} from "react-router-dom";
-import {useEffect} from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 
 interface Page {
@@ -34,10 +34,15 @@ function App() {
         <Route key={index} path={page.path} element={page.component && <page.component />} />
     ));
     routes.push(<Route key="404" path="*" element={<NotFoundPage />} />);
+
+    const basename = import.meta.env.DEV ? "/" : "/react-vite-gh-pages/";
+
     return (
         <div data-bs-theme="dark">
-            <Navbar title="Maxim Fiedler" active={0} pages={pages} onSelectItem={()=> null}></Navbar>
-            <Routes>{routes}</Routes>
+            <Navbar title="Maxim Fiedler" active={0} pages={pages} onSelectItem={() => null} />
+            <BrowserRouter basename={basename}>
+                <Routes>{routes}</Routes>
+            </BrowserRouter>
         </div>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig, UserConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }): UserConfig => {
+  const config: UserConfig = {
+    plugins: [react()],
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/about-me/';
+  }
+
+  return config;
+});
