f78080a61bd489626c95cea7aa7c35029643108a
Maxim <maximdeclient@gmail.com>
2023-07-11 13:31:29 +0200

    Added contact information to the ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,11 +1,30 @@
+import { FaEnvelope, FaDiscord, FaGithub } from 'react-icons/fa';
+
 
 function ContactPage() {
     return (
         <div>
             <h1 className={"page-title"}>Contact</h1>
-            {}
+            <ContactInformation />
         </div>
     );
 }
+
+function ContactInformation() {
+    return (
+        <div className="center-content">
+            <p className="contact-paragraph">
+                <FaEnvelope /> Email <a href="mailto:contact@maximfiedler.com">contact@maximfiedler.com</a>
+            </p>
+            <p className="contact-paragraph">
+                <FaDiscord /> Discord <a href="https://discord.com/users/759334613335670805">maximde</a>
+            </p>
+            <p className="contact-paragraph">
+                <FaGithub /> Github <a href="https://github.com/MaximFiedler">MaximFiedler</a>
+            </p>
+        </div>
+    );
+}
+
 
 export default ContactPage;
